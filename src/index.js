4efb770f5753b17ed8936fe8378144e7a7e389af
github username <github email address>
2020-02-10 11:03:05 -0500

    Streak incremements and resets when streak breaks

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ fetchQuestion()
 answerButton().addEventListener('click', checkValue)
 })
 
+// global variable to access question object
 let questionObject
 
 function fetchQuestion() {
@@ -20,6 +21,7 @@ function getQuestionDiv(){
   return document.querySelector('.question-div')
 }
 
+// renders a single question
 function renderQuestion(questionData) {
   questionObject = questionData
   let btnDiv = document.getElementById('button-div')
@@ -35,22 +37,40 @@ function answerButton() {
   return document.getElementById('button-div')
 }
 
+function streakCounter() {
+return document.getElementById('streak')
+}
 
+// checks to see if the user selected the correct answer
 function checkValue(e) {
+  // debugger
   let rightAnswer = questionObject.correct_answer
-  console.log(rightAnswer)
+  // console.log(rightAnswer)
   if (rightAnswer == e.target.value){
-  alert("You're Right")
+    // change to alert once we are finished testing
+  console.log("You're Right")
+  addToStreak()
 } else {
-  alert('Sorry, thats incorrect')
+  console.log('Sorry, thats incorrect')
+
+  resetStreak()
   }
+
   let questionP = document.getElementById('question-text')
   questionP.innerHTML = ""
   fetchQuestion()
 }
 
-  //take in result of true/false button selection
 
-//compare result to correct answer
+// if the user selects the correct answer, increment the "streak" by 1
+  function addToStreak() {
+    let newStreak = parseInt(streakCounter().innerText) + 1
+    streakCounter().innerText = newStreak
+  }
 
-  //if result and answer is true, answer is correct
+  // resets the streak count if user selects the wrong answer
+  function resetStreak(e) {
+    
+    console.log(`You got ${streakCounter().innerText} answers right in a row`)
+    streakCounter().innerText = 0
+  }
